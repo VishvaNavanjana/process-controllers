8da3cfb11d23f26ae442db11f1935bd526661eab
thisaramanohara <thisaramanohara@gmail.com>
2022-09-26 11:00:23 +0530

    Fake humidity sensor done

---
+++ humidity_sensor_fake.py
@@ -0,0 +1,16 @@
+import paho.mqtt.client as mqtt
+import random
+import time
+
+mqttBroker = "mqtt.eclipseprojects.io"
+client =mqtt.Client("Thermostat")
+client.connect(mqttBroker)
+
+topic = "326/humidity"
+
+while True:
+    humidity = random.uniform(0, 100)
+    client.publish(topic, humidity)
+    print("published " + str(humidity) + "to topic " + topic)
+    time.sleep(3)
+
