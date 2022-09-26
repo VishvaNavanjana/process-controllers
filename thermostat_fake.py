8674bc24b1af229daec7d734929653723c376289
thisaramanohara <thisaramanohara@gmail.com>
2022-09-26 10:25:33 +0530

    Fake thermostat ok

---
+++ thermostat_fake.py
@@ -0,0 +1,16 @@
+import paho.mqtt.client as mqtt
+import random
+import time
+
+mqttBroker = "mqtt.eclipseprojects.io"
+client =mqtt.Client("Thermostat")
+client.connect(mqttBroker)
+
+topic = "326/temp"
+
+while True:
+    temperature = random.uniform(0, 100)
+    client.publish(topic, temperature)
+    print("published " + str(temperature) + "to topic " + topic)
+    time.sleep(3)
+
