ef3866265eaa348ae62af56e3c85a85fb6d84e78
thisaramanohara <thisaramanohara@gmail.com>
2022-09-26 10:44:09 +0530

    Temperature control ok

--- temp-humid-control.py
+++ temp-humid-control.py
@@ -7,13 +7,29 @@ mqttBroker = "mqtt.eclipseprojects.io"
 client = mqtt.Client("temp-humid-controller")
 client.connect(mqttBroker)
 
-tempTopic = "326/temp"
+# for temperature
+tempSensorTopic = "326/temp"
+tempControlTopic = "326/control/temp"
+
+tempThreashold = 32
+tempCanChange = 2
 
 def on_message(client, userdata, message):
     print("Received " + str(message.payload.decode("utf-8")))
+    if (float(message.payload.decode("utf-8")) < (tempThreashold - tempCanChange)):
+        client.publish(tempControlTopic, "Provide Hot Air")
+        print("published 'Provide Hot Air' to topic " + tempControlTopic)
+
+    elif (float(message.payload.decode("utf-8")) > (tempThreashold + tempCanChange)):
+        client.publish(tempControlTopic, "Provide Cold Air")
+        print("published 'Provide Cold Air' to topic " + tempControlTopic)
+
+    else:
+        print("Maintainig current temperature levels")
+
 
 def run():
-    client.subscribe(tempTopic)
+    client.subscribe(tempSensorTopic)
     client.on_message = on_message
     time.sleep(1)
     client.loop_forever()
