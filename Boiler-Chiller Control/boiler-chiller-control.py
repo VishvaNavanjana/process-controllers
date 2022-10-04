0ee1d27104a4926ea43bdce6d490258b61714009
VishvaNavanjana <vishvanavanjana@gmail.com>
2022-10-04 14:37:29 +0530

    function for hot air duct controller added and tested

--- Boiler-Chiller Control/boiler-chiller-control.py
+++ Boiler-Chiller Control/boiler-chiller-control.py
@@ -41,8 +41,37 @@ def on_message_for_cold_air_duct(client, userdata, message):
         print("published 'Chiller OFF' to topic " + tempColdAirControlTopic)
 
     print()
+    
+
+def on_message_for_hot_air_duct(client, userdata, message):
+    data = json.loads(message.payload)
+    # print(data)
+    
+    # data validation
+    length = len(data)
+    keys = list(data.keys())
+    values = list(data.values())
+    if (length != 2 or keys[0] != 'time' or keys[1] != 'temp'):
+        return
+    
+    temperature = values[1]
+    print("Received Temperature " + str(temperature))
+    
+    if (temperature < (tempHotAirThreashold - tempCanChange)):
+        client.publish(tempHotAirControlTopic , "Boiler ON")
+        print("published 'Boiler ON' to topic " + tempHotAirControlTopic)
+    #on desired temparatures
+    elif (temperature < (tempHotAirThreashold + tempCanChange) and  temperature > (tempColdAirThreashold - tempCanChange)):
+        client.publish(tempHotAirControlTopic , "Boiler OFF")
+        print("published 'Boiler OFF' to topic " + tempHotAirControlTopic)
+
+    print()
+
+
+
 
 client.message_callback_add(tempColdAirSensorTopic, on_message_for_cold_air_duct)
+client.message_callback_add(tempHotAirSensorTopic, on_message_for_hot_air_duct)
 client.connect(mqttBroker, port=8883)
 client.subscribe("326/sensor/#")
 client.loop_forever()
