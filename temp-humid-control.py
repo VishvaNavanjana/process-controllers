acf6e8c9908c81cf0fc96679b7006620a579fce7
thisaramanohara <thisaramanohara@gmail.com>
2022-10-05 14:12:01 +0530

    Removed unwanted

--- temp-humid-control.py
+++ temp-humid-control.py
@@ -49,20 +49,6 @@ def on_message_for_temp(client, userdata, message):
     print()
 
 
-    # print("Received Temperature " + str(message.payload.decode("utf-8")))
-    #
-    # if (float(message.payload.decode("utf-8")) < (tempThreashold - tempCanChange)):
-    #     client.publish(tempControlTopic, "Provide Hot Air")
-    #     print("published 'Provide Hot Air' to topic " + tempControlTopic)
-    #
-    # elif (float(message.payload.decode("utf-8")) > (tempThreashold + tempCanChange)):
-    #     client.publish(tempControlTopic, "Provide Cold Air")
-    #     print("published 'Provide Cold Air' to topic " + tempControlTopic)
-    #
-    # else:
-    #     client.publish(tempControlTopic, "Turn OFF")
-    #     print("Maintainig current temperature levels")
-
 # controlling humidity
 def on_message_for_humid(client, userdata, message):
 
@@ -95,22 +81,6 @@ def on_message_for_humid(client, userdata, message):
     print()
 
 
-
-
-# client.message_callback_add(tempSensorTopic, on_message_for_temp)
-# client.connect(mqttBroker)
-# client.subscribe(tempSensorTopic)
-# client.loop_forever()
-#
-# client.message_callback_add(humidSensorTopic, on_message_for_humid)
-# client.connect(mqttBroker)
-# client.subscribe(humidSensorTopic)
-# client.loop_forever()
-
-
-# def on_message(client, uerdata, message):
-#     print(message.topic + " " + str(message.qos) + " " + str(message.payload))
-#     print()
 #
 client.message_callback_add(tempSensorTopic, on_message_for_temp)
 client.message_callback_add(humidSensorTopic, on_message_for_humid)
@@ -118,34 +88,7 @@ client.connect("vpn.ce.pdn.ac.lk", port=8883)
 client.subscribe("326/sensor/#")
 client.loop_forever()
 #
-# client.loop_forever()
 
-
-# def run():
-#     client.subscribe(tempSensorTopic)
-#     client.on_message = on_message_for_temp
-#     time.sleep(5)
-#     client.subscribe(humidSensorTopic)
-#     client.on_message = on_message_for_humid
-#     time.sleep(5)
-#     client.loop_forever()
-#
-# run()
-
-
-# def run():
-#     client.subscribe(tempSensorTopic, 2)
-#     client.subscribe(humidSensorTopic, 2)
-#     client.message_callback_add(tempSensorTopic, on_message_for_temp)
-#     # time.sleep(5)
-#     client.message_callback_add(humidSensorTopic, on_message_for_humid)
-#     # time.sleep(5)
-#     # client.on_message = on_message
-#
-#     time.sleep(5)
-#     client.loop_forever()
-#
-# run()
 
 
 
