a11a064d605119880015683e2a190e7e6a2d7c3d
thisaramanohara <thisaramanohara@gmail.com>
2022-10-12 12:40:22 +0530

    Blower controller done

--- Blower-controller/blower_control.py
+++ Blower-controller/blower_control.py
@@ -9,13 +9,11 @@ client = mqtt.Client("blower-controller")
 
 # for temperature
 tempSensorTopic = "326project/smartbuilding/hvac/sensor/temperature/floorX/roomX"
-# tempControlTopic = "326project/smartbuilding/hvac/control/boiler/"
 tempThreashold = 32
 tempCanChange = 2
 
 # for humidity
 humidSensorTopic = "326project/smartbuilding/hvac/sensor/humidity/floorX/roomX"
-# humidControlTopic = "326project/smartbuilding/hvac/control/chiller/"
 humidThreashold = 65
 humidCanChange = 2
 
@@ -41,34 +39,6 @@ def on_message_for_temp(client, userdata, message):
 
     create_blower_control_command(tempPrevious, humidityPrevious)
 
-    #
-    #
-    # print("Received Temperature " + str(temperature))
-    #
-    # if (temperature < (tempThreashold - tempCanChange)):
-    #
-    #     x = {
-    #         "time": datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S'),
-    #         "state": 1
-    #     }
-    #     client.publish(tempControlTopic, json.dumps(x))
-    #     print("published 'Provide Hot Air' to topic " + tempControlTopic)
-    #
-    # elif (temperature > (tempThreashold + tempCanChange)):
-    #
-    #     x = {
-    #         "time": datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S'),
-    #         "state": 0
-    #     }
-    #     client.publish(tempControlTopic, json.dumps(x))
-    #     print("published 'Provide Cold Air' to topic " + tempControlTopic)
-    #
-    # else:
-    #     client.publish(tempControlTopic, "Turn OFF")
-    #     print("Maintainig current temperature levels")
-    #
-    # print()
-
 
 # controlling humidity
 def on_message_for_humid(client, userdata, message):
@@ -87,43 +57,8 @@ def on_message_for_humid(client, userdata, message):
 
     create_blower_control_command(tempPrevious, humidityPrevious)
 
-    # print("Received Humidity " + str(humidity))
-    #
-    # if (humidity < (humidThreashold - humidCanChange)):
-    #
-    #     x = {
-    #         "time": datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S'),
-    #         "state": 1
-    #     }
-    #     client.publish(tempControlTopic, json.dumps(x))
-    #     print("published 'Increase Humidity' to topic " + humidControlTopic)
-    #
-    # elif (humidity > (tempThreashold + tempCanChange)):
-    #
-    #     x = {
-    #         "time": datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S'),
-    #         "state": 0
-    #     }
-    #     client.publish(tempControlTopic, json.dumps(x))
-    #     print("published 'Decrease Humidity' to topic " + humidControlTopic)
-    #
-    # else:
-    #     client.publish(humidControlTopic, "Turn OFF")
-    #     print("Maintainig current Humidity levels")
-    #
-    # print()
 
 def create_blower_control_command(temp, humid):
-
-    # x = {
-    #     "time": datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S'),
-    #     "speed": 0.5
-    # }
-    # client.publish(blowerControlTopic, json.dumps(x))
-    # print("published 'Provide Hot Air' to topic " + blowerControlTopic)
-    #
-    # print()
-
 
     # if temp and humid are higher
     if ((tempThreashold+tempCanChange) < temp):
